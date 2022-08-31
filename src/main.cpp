1f8c06bba3083a8a3de07cc88d1809e7cce39280
João Pedro Assis <joaopedroassisdossantos@gmail.com>
2022-08-31 11:31:41 -0300

    uses stream buffers to communicate between tasks

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,22 @@
 #include "BluetoothA2DPSink.h"
 #include <FastLED.h>
 #include <arduinoFFT.h>
+#include <stdint.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/stream_buffer.h>
 
 // LED settings
 #define NUM_LEDS 60
-#define LED_PIN 4
+#define LED_PIN 32
 #define INIT_BRIGHTNESS 100
+
+// FFT
+#define NUM_BANDS 16
+#define SAMPLES 1024
+#define SAMPLING_FREQUENCY 44100
+#define NOISE 500
+
+#define BUFFER_SIZE 3
 
 CRGB leds[NUM_LEDS];
 
@@ -17,114 +28,52 @@ DEFINE_GRADIENT_PALETTE( greenblue_gp ) {
   255, 0,  255, 245
 };
 CRGBPalette16 greenblue = greenblue_gp;
-
-// FFT
-#define NUM_BANDS 16
-#define SAMPLES 1024
-#define SAMPLING_FREQUENCY 44100
-#define NOISE 500
-
-arduinoFFT FFT = arduinoFFT();
 BluetoothA2DPSink a2dp_sink;
+StreamBufferHandle_t sample_buffer;
 
-float amplitude = 200.0;
+double *tmp_buffer;
 
-int32_t peak[NUM_BANDS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-double vReal[SAMPLES];
-double vImag[SAMPLES];
-
-// Multitask stuff
-QueueHandle_t bl_queue;
-
-void createBands(int i, int dsize) {
-  uint8_t band = 0;
-
-  if (i<=2 )           band = 1;
-  if (i>2   && i<=3  ) band = 2;
-  if (i>3   && i<=5  ) band = 3;
-  if (i>5   && i<=7  ) band = 4;
-  if (i>7   && i<=9  ) band = 5;
-  if (i>9   && i<=13 ) band = 6;
-  if (i>13  && i<=18 ) band = 7;
-  if (i>18  && i<=25 ) band = 8;
-  if (i>25  && i<=36 ) band = 9;
-  if (i>36  && i<=50 ) band = 10;
-  if (i>50  && i<=69 ) band = 11;
-  if (i>69  && i<=97 ) band = 12;
-  if (i>97  && i<=135) band = 13;
-  if (i>135 && i<=189) band = 14;
-  if (i>189 && i<=264) band = 15;
-
-  int dmax = amplitude;
-  if (dsize > dmax)
-    dsize = dmax;
-  if (dsize > peak[band]) {
-    peak[band] = dsize;
-  }
-}
-
-void read_data_stream(const uint8_t *data, uint32_t length) {
-  int item = 0;
+void read_data_stream(const uint8_t *data, uint32_t length) { 
   int16_t l_sample, r_sample;
+  int byte_offset = 0;
+  size_t buffer_byte_size = SAMPLES * sizeof(double);
+  size_t bytes_sent;
 
-  if (uxQueueMessagesWaiting(bl_queue) == 0) {
-    int byte_offset = 0;
+  for (int i = 0; i < SAMPLES; i++) {
+    l_sample = (int16_t)(((*(data + byte_offset + 1) << 8) | *(data + byte_offset)));
+    r_sample = (int16_t)(((*(data + byte_offset + 3) << 8) | *(data + byte_offset + 2)));
 
-    for (int i = 0; i < SAMPLES; i++) {
-      l_sample = (int16_t)(((*(data + byte_offset + 1) << 8) | *(data + byte_offset)));
-      r_sample = (int16_t)(((*(data + byte_offset + 3) << 8) | *(data + byte_offset + 2)));
+    tmp_buffer[i] = (l_sample + r_sample) / 2.0f;
+    byte_offset = byte_offset + 4;
+  }
 
-      vReal[i] = (l_sample + r_sample) / 2.0f;
-      vImag[i] = 0;
-      byte_offset = byte_offset + 4;
-    }
-
-    xQueueSend(bl_queue, &item, portMAX_DELAY);
+  bytes_sent = xStreamBufferSend(sample_buffer, tmp_buffer, buffer_byte_size, 0);
+  if (bytes_sent != buffer_byte_size) {
+    Serial.println("Nem todos os bytes foram enviados!");
+  } else {
+    Serial.println("Todos os bytes foram enviados!");
   }
 }
 
 void calculateFFT(void *parameters) {
-  int item = 0;
+  size_t bytes_received;
+  size_t buffer_byte_size = SAMPLES * BUFFER_SIZE * sizeof(double);
+  double *fft_buffer = (double*) malloc(buffer_byte_size);
   while (1) {
-    if (uxQueueMessagesWaiting(bl_queue) > 0) {
+    bytes_received = xStreamBufferReceive(sample_buffer, fft_buffer, buffer_byte_size, portMAX_DELAY);
 
-      FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-      FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-      FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
-
-      for (uint8_t band = 0; band < NUM_BANDS; band++) {
-        peak[band] = 0;
-      }
-
-      for (int i = 2; i < (SAMPLES / 2); i++) {
-        if (vReal[i] > NOISE) { // Add a crude noise filter, 10 x amplitude or more
-          createBands(i, (int)vReal[i] / amplitude);
-        }
-      }
-
-      xQueueReceive(bl_queue, &item, 0);
-
-      int32_t max_peak = peak[0];
-      int max_peak_idx = 0;
-      for (int i = 0; i < NUM_BANDS; i++) {
-        if (peak[i] > max_peak) {
-          max_peak = peak[i];
-          max_peak_idx = i;
-        }
-      }
-
-      Serial.println(max_peak_idx);
-
-      int led_brightness = map(max_peak_idx, 0, 15, 0, 255);
-      // fill_solid(leds, NUM_LEDS, CHSV(led_brightness, 255, 255));
-      FastLED.setBrightness(led_brightness);
-      FastLED.show();
+    if (bytes_received != buffer_byte_size) {
+      Serial.println("Nem todos os bytes foram recebidos!");
+    } else {
+      Serial.println("Todos os bytes foram recebidos!");
     }
   }
 }
 
 
 void setup() {
+
+  Serial.begin(115200);
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(INIT_BRIGHTNESS);
@@ -147,27 +96,34 @@ void setup() {
     .use_apll = false
   };
 
-  bl_queue = xQueueCreate(1, sizeof(int));
-
   xTaskCreatePinnedToCore(
     calculateFFT,
     "FFT",
-    10000,
+    25000,
     NULL,
     1,
     NULL,
     1
   );
 
+  tmp_buffer = (double*) malloc(SAMPLES * sizeof(double));
+
+  int stream_size = SAMPLES * BUFFER_SIZE * sizeof(double);
+  sample_buffer = xStreamBufferCreate(stream_size, stream_size);
+
+  if( sample_buffer == NULL ) {
+    Serial.println("SAMPLE BUFFER NOT INITIALIZED");
+  } else {
+    Serial.println("SAMPLE BUFFER INITIALIZED");
+  }
+
   a2dp_sink.set_i2s_config(i2s_config);
   a2dp_sink.start("JP-Bluetooth");
   a2dp_sink.set_stream_reader(read_data_stream);
 
-  Serial.begin(115200);
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
 }
